156c319ab9036f9b86429a20bc1ae6d8b5656444
xlboy <xlboy88888@foxmail.com>
2023-02-16 12:19:18 +0800

    feat(recursion/part1): add `binarySearch`

--- src/recursion/part1.ts
+++ src/recursion/part1.ts
@@ -10,11 +10,11 @@ function sumOfNumbers(numbers: Array<number>, currentSum = 0): number {
 }
 
 function sum(numbers: Array<number>): number {
-    if (numbers.length === 1) {
-        return numbers[0]
-    } else {
-        return numbers[0] + sum(numbers.slice(1))
-    }
+  if (numbers.length === 1) {
+    return numbers[0];
+  } else {
+    return numbers[0] + sum(numbers.slice(1));
+  }
 }
 
 test("sumOfNumbers", () => {
@@ -22,8 +22,8 @@ test("sumOfNumbers", () => {
 });
 
 test("sum", () => {
-    expect(sum([55, 1, 9, 1, 88, 0])).toBe(154);
-})
+  expect(sum([55, 1, 9, 1, 88, 0])).toBe(154);
+});
 
 function findMaxNumber(numbers: Array<number>, currentMax = 0): number {
   // 基线条件：数组长度为 1；递归条件：数组长度大于 1
@@ -36,4 +36,31 @@ function findMaxNumber(numbers: Array<number>, currentMax = 0): number {
 
 test("findMaxNumber", () => {
   expect(findMaxNumber([55, 1, 9, 1, 88, 0])).toBe(88);
+});
+
+function binarySearch(
+  numbers: Array<number>,
+  target: number,
+  left = 0,
+  right = numbers.length - 1
+): number {
+  // 基线条件：left > right
+  if (left > right) {
+    return -1;
+  }
+
+  const middle = Math.floor((left + right) / 2);
+  const middleValue = numbers[middle];
+
+  if (middleValue === target) {
+    return middle;
+  } else if (middleValue > target) {
+    return binarySearch(numbers, target, left, middle - 1);
+  } else {
+    return binarySearch(numbers, target, middle + 1, right);
+  }
+}
+
+test("binarySearch", () => {
+  expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)).toBe(2);
 });
